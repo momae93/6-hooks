1f4596bf9df459079046f6fdd01a5ed7fbe8fc1c
Mickael AU <mickael.au@gmail.com>
2018-12-10 17:35:41 +0100

    [POSTLIST] Fixed fetch api

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Post from './Post';
 
-const PostsList = ({ posts = [] }) => (
-  <ul style={{ listStyle: 'none', width: '800px' }}>
-    {posts.map(p => (
-      <Post data={p} />
-    ))}
-  </ul>
-);
+
+// Use effect : we need to put an array at the end to stop the fetching
+// Appeler l'url
+const PostsList = ({ posts = [] }) => {
+  const [fetchedPosts, setFetchedPosts] = useState(posts);
+
+  useEffect(() => {
+    const url = 'https://jsonplaceholder.typicode.com/posts';
+
+    fetch(url)
+      .then((data) => data.json())
+      .then((data) => setFetchedPosts(data))
+      .catch(() => console.log('Sorry an error occured !'));
+  }, []);
+
+  return (
+    <ul style={{ listStyle: 'none', width: '800px' }}>
+      {fetchedPosts.map(p => (
+        <Post data={p} />
+      ))}
+    </ul>
+  );
+}
 
 export default PostsList;
