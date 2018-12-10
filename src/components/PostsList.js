4cedeb96633a488a5096145a29f555e32ca25320
Mickael AU <mickael.au@gmail.com>
2018-12-10 18:02:52 +0100

    [POSTLIST] Fixed postlist

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Post from './Post';
 
 
@@ -6,6 +6,9 @@ import Post from './Post';
 // Appeler l'url
 const PostsList = ({ posts = [] }) => {
   const [fetchedPosts, setFetchedPosts] = useState(posts);
+  const inputTitle = useRef(null);
+  const inputBody = useRef(null);
+  const inputUserId = useRef(null);
 
   useEffect(() => {
     const url = 'https://jsonplaceholder.typicode.com/posts';
@@ -16,12 +19,29 @@ const PostsList = ({ posts = [] }) => {
       .catch(() => console.log('Sorry an error occured !'));
   }, []);
 
+  const onSubmitPost = () => {
+    const post = {
+      "userId": inputUserId.current.value,
+      "title": inputTitle.current.value,
+      "body": inputBody.current.value
+    }
+    setFetchedPosts([post, ...fetchedPosts])
+  };
+
   return (
-    <ul style={{ listStyle: 'none', width: '800px' }}>
-      {fetchedPosts.map(p => (
-        <Post data={p} />
-      ))}
-    </ul>
+    <div>
+      <div>
+        <input ref={inputTitle} type="text" />
+        <input ref={inputBody} type="text" />
+        <input ref={inputUserId} type="text" />
+        <button onClick={onSubmitPost}>Add</button>
+      </div>
+      <ul style={{ listStyle: 'none', width: '800px' }}>
+        {fetchedPosts.map(p => (
+          <Post data={p} />
+        ))}
+      </ul>
+    </div>
   );
 }
 
